2ff2264bc6849f4d6a96ecad02e92ee8ca0c9945
wwwgong <wen.g.gong@gmail.com>
2022-01-21 19:36:32 -0500

    upd

--- demos/demo_concept.py
+++ demos/demo_concept.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 # import matplotlib.pyplot as plt
 import altair as alt
-import plotly.figure_factory as ff
+# import plotly.figure_factory as ff
 # from bokeh.plotting import figure
 
 import streamlit as st
@@ -94,22 +94,22 @@ def demo_chart():
         x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
     st.altair_chart(c, use_container_width=True)
 
-    st.subheader('st.plotly_chart') 
-    # Add histogram data
-    x1 = np.random.randn(200) - 2
-    x2 = np.random.randn(200)
-    x3 = np.random.randn(200) + 2
+    # st.subheader('st.plotly_chart') 
+    # # Add histogram data
+    # x1 = np.random.randn(200) - 2
+    # x2 = np.random.randn(200)
+    # x3 = np.random.randn(200) + 2
 
-    # Group data together
-    hist_data = [x1, x2, x3]
+    # # Group data together
+    # hist_data = [x1, x2, x3]
 
-    group_labels = ['Group 1', 'Group 2', 'Group 3']
+    # group_labels = ['Group 1', 'Group 2', 'Group 3']
 
-    # Create distplot with custom bin_size
-    fig = ff.create_distplot(
-            hist_data, group_labels, bin_size=[.1, .25, .5])
+    # # Create distplot with custom bin_size
+    # fig = ff.create_distplot(
+    #         hist_data, group_labels, bin_size=[.1, .25, .5])
 
-    st.plotly_chart(fig, use_container_width=True)
+    # st.plotly_chart(fig, use_container_width=True)
 
     st.subheader('st.graphviz_chart') 
     st.graphviz_chart('''
