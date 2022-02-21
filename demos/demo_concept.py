1dc515a5b0c5f17f63282cb770e942f93246d03a
wwwgong <wen.g.gong@gmail.com>
2022-02-20 21:58:44 -0500

    add footer demo

--- demos/demo_concept.py
+++ demos/demo_concept.py
@@ -29,6 +29,11 @@ import altair as alt
 # from bokeh.plotting import figure
 import pydeck as pdk
 
+from streamlit.components.v1 import html
+from htbuilder import HtmlElement, div, ul, li, br, hr, a, p, img, styles, classes, fonts
+from htbuilder.units import percent, px
+from htbuilder.funcs import rgba, rgb
+
 # Initial page config
 st.set_page_config(
      page_title='Streamlit Concept Demo',
@@ -885,49 +890,55 @@ def do_nothing():
     pass
 
 
-def _tabs(tabs_data = {}, default_tab=0):
-        tab_titles = list(tabs_data.keys())
-        if not tab_titles:
-            return None
-        active_tab = st.radio("", tab_titles, index=default_tab)
-        tab_idx = tab_titles.index(active_tab)+1
-        st.markdown("""  
-            <style type="text/css">
-            div[role=radiogroup] > label > div:first-of-type {
-               display: none
-            }
-            div[role=radiogroup] {
-                flex-direction: unset
-            }
-            div[role=radiogroup] label {             
-                border: 1px solid #999;
-                background: #FFF;
-                padding: 6px 12px;
-                border-radius: 12px 12px 0 0;
-                position: relative;
-                top: 1px;
-                }
-            div[role=radiogroup] label:nth-child(""" + str(tab_idx) + """) {    
-                background:  #CCFF00 !important;
-                border-bottom: 1px solid transparent;
-            }            
-            </style>
-        """,unsafe_allow_html=True)        
-        return tabs_data[active_tab]
 
-def _tab__show_video():
-    st.title("Russia – Ukraine conflict / crisis Explained")
-    st.video("https://www.youtube.com/watch?v=h2P9AmGcMdM")
-
-def _tab__fake_df():
-    N = 50
-    rand = pd.DataFrame()
-    rand['a'] = np.arange(N)
-    rand['b'] = np.random.rand(N)
-    rand['c'] = np.random.rand(N)    
-    return rand
 
 def do_tabs():
+    """
+    Thank you - https://discuss.streamlit.io/t/st-footer/6447/18
+    """
+
+    def _tabs(tabs_data = {}, default_tab=0):
+            tab_titles = list(tabs_data.keys())
+            if not tab_titles:
+                return None
+            active_tab = st.radio("", tab_titles, index=default_tab)
+            tab_idx = tab_titles.index(active_tab)+1
+            st.markdown("""  
+                <style type="text/css">
+                div[role=radiogroup] > label > div:first-of-type {
+                display: none
+                }
+                div[role=radiogroup] {
+                    flex-direction: unset
+                }
+                div[role=radiogroup] label {             
+                    border: 1px solid #999;
+                    background: #FFF;
+                    padding: 6px 12px;
+                    border-radius: 12px 12px 0 0;
+                    position: relative;
+                    top: 1px;
+                    }
+                div[role=radiogroup] label:nth-child(""" + str(tab_idx) + """) {    
+                    background:  #CCFF00 !important;
+                    border-bottom: 1px solid transparent;
+                }            
+                </style>
+            """,unsafe_allow_html=True)        
+            return tabs_data[active_tab]
+
+    def _tab__show_video():
+        st.title("Russia – Ukraine conflict / crisis Explained")
+        st.video("https://www.youtube.com/watch?v=h2P9AmGcMdM")
+
+    def _tab__fake_df():
+        N = 50
+        rand = pd.DataFrame()
+        rand['a'] = np.arange(N)
+        rand['b'] = np.random.rand(N)
+        rand['c'] = np.random.rand(N)    
+        return rand
+
     st.markdown("This __Tab__ example was found at [Multiple tabs in streamlit](https://discuss.streamlit.io/t/multiple-tabs-in-streamlit/1100/19?u=wgong27514)")
     tab_content = _tabs({
             "Tab html": "<h2> Hello Streamlit, <br/> what a cool tool! </h2>",
@@ -944,6 +955,149 @@ def do_tabs():
     else:
         st.write(tab_content) 
 
+    with st.expander("Show code"):
+        st.code(inspect.getsource(do_tabs))
+
+
+
+
+
+
+def do_footer():
+    """
+    Thank you - https://discuss.streamlit.io/t/st-footer/6447/18?u=wgong27514
+    """
+
+    def _image(src_as_string, **style):
+        return img(src=src_as_string, style=styles(**style))
+
+
+    def _link(link, text, **style):
+        return a(_href=link, _target="_blank", style=styles(**style))(text)
+
+
+    def _layout(*args):
+        style = """
+        <style>
+        # MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        .stApp { bottom: 80px; }
+        </style>
+        """
+
+        style_div = styles(
+            position="fixed",
+            left=0,
+            bottom=0,
+            margin=px(0, 0, 0, 0),
+            width=percent(100),
+            color="black",
+            text_align="center",
+            height="auto",
+            opacity=1
+        )
+
+        # 分割线
+        style_hr = styles(
+            display="block",
+            margin=px(0, 0, 0, 0),
+            border_style="inset",
+            border_width=px(2)
+        )
+
+        # 修改p标签内文字的style
+        body = p(
+            id='myFooter',
+            style=styles(
+                margin=px(0, 0, 0, 0),
+                # 通过调整padding自行调整上下边距以达到满意效果
+                padding=px(5),
+                # 调整字体大小
+                font_size="0.8rem",
+                color="rgb(51,51,51)"
+            )
+        )
+        foot = div(
+            style=style_div
+        )(
+            hr(
+                style=style_hr
+            ),
+            body
+        )
+
+        st.markdown(style, unsafe_allow_html=True)
+
+        for arg in args:
+            if isinstance(arg, str):
+                body(arg)
+
+            elif isinstance(arg, HtmlElement):
+                body(arg)
+
+        st.markdown(str(foot), unsafe_allow_html=True)
+
+        # js获取背景色 由于st.markdown的html实际上存在于iframe, 所以js检索的时候需要window.parent跳出到父页面
+        # 使用getComputedStyle获取所有stApp的所有样式，从中选择bgcolor
+        js_code = '''
+        <script>
+        function rgbReverse(rgb){
+            var r = rgb[0]*0.299;
+            var g = rgb[1]*0.587;
+            var b = rgb[2]*0.114;
+            
+            if ((r + g + b)/255 > 0.5){
+                return "rgb(49, 51, 63)"
+            }else{
+                return "rgb(250, 250, 250)"
+            }
+            
+        };
+        var stApp_css = window.parent.document.querySelector("#root > div:nth-child(1) > div > div > div");
+        window.onload = function () {
+            var mutationObserver = new MutationObserver(function(mutations) {
+                    mutations.forEach(function(mutation) {
+                        /************************当DOM元素发送改变时执行的函数体***********************/
+                        var bgColor = window.getComputedStyle(stApp_css).backgroundColor.replace("rgb(", "").replace(")", "").split(", ");
+                        var fontColor = rgbReverse(bgColor);
+                        var pTag = window.parent.document.getElementById("myFooter");
+                        pTag.style.color = fontColor;
+                        /*********************函数体结束*****************************/
+                    });
+                });
+                
+                /**Element**/
+                mutationObserver.observe(stApp_css, {
+                    attributes: true,
+                    characterData: true,
+                    childList: true,
+                    subtree: true,
+                    attributeOldValue: true,
+                    characterDataOldValue: true
+                });
+        }
+        
+
+        </script>
+        '''
+        html(js_code)
+
+    myargs = [
+        "Made with ❤️ by ",
+        _image('https://image.pngaaa.com/798/5084798-middle.png',
+              width=px(50), height=px(25)),
+        _link("http://streamlit.io", "Streamlit"),
+    ]
+    _layout(*myargs)
+
+    st.markdown("This __Footer__ example was found at [st.footer](https://discuss.streamlit.io/t/st-footer/6447/18)")
+
+    st.write("Do you see footer at the bottom? ")
+    st.image("https://hotemoji.com/images/emoji/l/17zcrtb11cx2zl.png")
+
+    with st.expander("Show code"):
+        st.code(inspect.getsource(do_footer))
+
 # menu_item to handler mapping
 menu_dict = {
     "demos" : {
@@ -952,6 +1106,7 @@ menu_dict = {
         "Mapping": {"fn": do_mapping, },
         "Plotting": {"fn": do_plotting},
         "Tab":  {"fn": do_tabs},
+        "Footer":  {"fn": do_footer},
     },
     "concepts" : {
             "Data": {"fn": do_data, },
